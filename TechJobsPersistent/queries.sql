1f7d2c8054326d271b1bed38aa7b85e64fc0df92
M. White <amnwhitejr@gmail.com>
2020-07-20 10:32:19 -0400

    additional submission files

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,10 @@
 --Part 1
-
+--id (int, PK), EmployerId(int), Name(varchar)
 --Part 2
+SELECT Name FROM Employers WHERE Location = "St. Louis City";
 
 --Part 3
-
+SELECT * FROM Skills
+	LEFT JOIN JobSkills ON Skill.Id = JobSkills.SkillId
+	WHERE JobSkills.JobId IS NOT NULL
+	ORDER BY name;
